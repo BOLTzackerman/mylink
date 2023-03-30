c401781fb821c70c786c7c58e6584a9c1cd10845
BOLTzackerman <129395030+BOLTzackerman@users.noreply.github.com>
2023-03-30 19:58:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//alert = untuk menampilkan taks di tengah web
+//...(" ")
+//prompt = pop up mengisi sesuatu
+//koma di javascript=.
+
+
+
+
+
+console.log("MY LINK");
+// let usia = prompt("berapa umur mu ?")
+// alert('umur mu adalah ' + usia )
+
+
+
+  
